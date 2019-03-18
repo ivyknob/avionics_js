7ed8e39789c243e66fe65afeaba9acb726d8d120
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2019-03-18 11:50:34 +0300

    Elongated zero pitch line

--- src/avionics/printPitch.js
+++ src/avionics/printPitch.js
@@ -24,27 +24,27 @@ export default function printPitch (elem) {
   };
   while (i <= 220) {
     if (i === 0) {
-      i += 2.5;
+      use = document.querySelector('#large-pitch').cloneNode();
+      use.setAttribute('x1', -60);
+      use.setAttribute('x2', 60);
     }
-    else {
-      if (i % 10 === 0) {
-        use = large.cloneNode();
-        texts = [
-          textLeft.cloneNode(),
-          textRight.cloneNode()
-        ];
-        texts.forEach(appendFn);
-      }
-      else if (i % 5 === 0) {
-        use = medium.cloneNode();
-      }
-      else if (i % 2.5 === 0) {
-        use = small.cloneNode();
-      }
-      use.setAttribute('y', i * 8);
-      elem.appendChild(use);
-      results.push(i += 2.5);
+    else if (i % 10 === 0) {
+      use = large.cloneNode();
+      texts = [
+        textLeft.cloneNode(),
+        textRight.cloneNode()
+      ];
+      texts.forEach(appendFn);
     }
+    else if (i % 5 === 0) {
+      use = medium.cloneNode();
+    }
+    else if (i % 2.5 === 0) {
+      use = small.cloneNode();
+    }
+    use.setAttribute('y', i * 8);
+    elem.appendChild(use);
+    results.push(i += 2.5);
   }
   return results;
 }
