57cfe780487021861854453fa91244db9c2aef6e
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2019-04-17 00:26:20 +0300

    Moved specs to other place

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'dist/avionics.js',
-      'dist/spec/all.js'
+      'spec/all.js'
     ],
 
 
